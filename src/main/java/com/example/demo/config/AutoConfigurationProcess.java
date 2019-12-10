812eb8043126139925b81a42eba56a61833be119
Patrick <patr1ck@126.com>
2019-12-10 18:00:37 +0800

    Add AutoConfigurationProcess

---
+++ src/main/java/com/example/demo/config/AutoConfigurationProcess.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AutoConfigurationProcess
+ *
+ * @author mawengang
+ * @date 2019/12/10 17:58
+ */
+@Configuration
+@Slf4j
+public class AutoConfigurationProcess {
+}
